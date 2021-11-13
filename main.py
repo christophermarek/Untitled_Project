814e5f9a1545c382bfde77e06f970d41d5bba70a
christophermarek <christophermarek42@gmail.com>
2021-11-13 16:51:08 -0500

    comparing libraries performance

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 
+from numpy import character, number
 import py_vollib
 from py_vollib.black_scholes_merton import black_scholes_merton
-from py_vollib.black_scholes_merton import implied_volatility
+from py_vollib.black_scholes_merton.implied_volatility import implied_volatility
+from py_vollib.black_scholes_merton.greeks.analytical import delta
+from py_vollib.black_scholes_merton.greeks.analytical import gamma
+from py_vollib.black_scholes_merton.greeks.analytical import rho
+from py_vollib.black_scholes_merton.greeks.analytical import theta
+from py_vollib.black_scholes_merton.greeks.analytical import vega
+
+import bs_calc
+
+def pyvolib(S, K, sigma, r, flag: character, t, q):
+    #IV and prices
+
+    # Calculate the Black-Scholes-Merton implied volatility.
+    price = black_scholes_merton(flag, S, K, t, r, sigma, q)
+    iv = implied_volatility(price, S, K, t, r, q, flag)
+    # Return the Black-Scholes-Merton option price.
+    p_calc = black_scholes_merton('p', S, K, t, r, sigma, q)
+
+    # Greeks
+    delta_calc = delta(flag, S, K, t, r, sigma, q)
+    gamma_calc = gamma(flag, S, K, t, r, sigma, q)
+    rho_calc = rho(flag, S, K, t, r, sigma, q)
+    # The text book analytical formula does not divide by 365, but in practice theta is defined as the change in price for each day change in t, hence we divide by 365
+    annual_theta_calc = theta(flag, S, K, t, r, sigma, q) * 365
+    vega_calc = vega(flag, S, K, t, r, sigma, q)
+
+    return delta_calc, gamma_calc, rho_calc, annual_theta_calc, vega_calc
 
 
 def main():
+    print("Begin Comparison")
 
     # S (float) – underlying asset price
     # K (float) – strike price
@@ -22,15 +50,9 @@ def main():
     t = .5
     q = 0
 
-    # Calculate the Black-Scholes-Merton implied volatility.
-    price = black_scholes_merton(flag, S, K, t, r, sigma, q)
-    iv = implied_volatility(price, S, K, t, r, q, flag)
-
-
-    # Return the Black-Scholes-Merton option price.
-    p_calc = black_scholes_merton('p', S, K, t, r, sigma, q)
-
-
-    print(price)
+    print(pyvolib(S, K, sigma, r, flag, t, q))
+    print(bs_calc.Black_Scholes_Greeks_Call(S, K, r, sigma, t, 0))
+    #They are identical
+    print("Comparison complete")
 
 main()
