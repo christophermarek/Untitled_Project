78c2aba86a32149d2df472b7fe346fff8174b168
christophermarek <christophermarek42@gmail.com>
2021-12-10 21:43:58 -0500

    some notes

--- neural_network.py
+++ neural_network.py
@@ -3,11 +3,11 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 from time import time
-from torchvision import datasets, transforms
 from torch import nn, optim
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-
+# https://digitalcommons.usu.edu/cgi/viewcontent.cgi?article=2513&context=gradreports
 def neural_network():
 
     # Get dataset
@@ -22,44 +22,37 @@ def neural_network():
     except:
         print('error opening file')
 
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('BS-Call', axis=1), 
+                                                    df['BS-Call'], test_size=0.2)
+
     # wonder if i should round output price column, we will see later
 
-    # Dataset needs an output column
-    
-    # # transforms.Normalize() — normalizes the tensor with a mean and standard deviation which goes as the two parameters respectively.
-    # transform = transforms.Compose([transforms.ToTensor(),
-    #                                 transforms.Normalize((0.5,), (0.5,)),
-    #                                 ])
+    # What type of neural network would i use
+    # linear for now simple
+      # The nn.Sequential wraps the layers in the network. There are three linear layers with ReLU activation ( a simple function which allows
+    # positive values to pass through, whereas negative values are modified to zero )
 
-    # # Now we finally download the data sets, shuffle them and transform each of them. We download the data sets and load them to DataLoader,
-    # # which combines the data-set and a sampler and provides single- or multi-process iterators over the data-set.
-    # This datasets.MNIST needs to be replace with whats in lecture
-    # trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET',
-    #                           download=True, train=True, transform=transform)
-    # valset = datasets.MNIST('PATH_TO_STORE_TESTSET',
-    #                         download=True, train=False, transform=transform)
-    # # In one line, batch size is the number of images we want to read in one go.
-    # trainloader = torch.utils.data.DataLoader(
-    #     trainset, batch_size=64, shuffle=True)
-    # valloader = torch.utils.data.DataLoader(
-    #     valset, batch_size=64, shuffle=True)
+    #      The input
+    # layer has six nodes (one for each input parameter). The hidden layers have ten nodes each. The
+    # output layer has one node (for the predicted price)
+    input_size = 6
+    # first hidden layer 128 neurons, seconq 64
+    hidden_sizes = [10, 10]
+    output_size = 1
+    # input data passes through the model in sequential order, linear[hl1] -> relu -> linear[hl2] -> relu -> output -> softmax
+    model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                        nn.ReLU(),
+                        nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                        nn.ReLU(),
+                        nn.Linear(hidden_sizes[1], output_size),
+                        nn.LogSoftmax(dim=1))
+    print(model)
 
-    # # Visualize data
-    # dataiter = iter(trainloader)
-    # images, labels = dataiter.next()
+    # https://stackoverflow.com/questions/46170814/how-to-train-pytorch-model-with-numpy-data-and-batch-size
+    # THis guy has a good way of setting up his code, and the guy answering explains how to do it right.
+    # he even links the docs, cause the data needs to be formatted a specific way for pytorhc
 
-    # print(images.shape)
-    # print(labels.shape)
-
-    # figure = plt.figure()
-
-    # num_of_images = 60
-    # for index in range(1, num_of_images + 1):
-    #     plt.subplot(6, 10, index)
-    #     plt.axis('off')
-    #     plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
-
-    # # random sample of images in trainingset
-    # plt.show()
-
+    # https://github.com/pytorch/examples/blob/master/mnist/main.py#L37
+    # THis official example I can just copy most of it, it looks right too and can load local files
+    # like the other tutorial did instead of a slow to load df.
 neural_network()
