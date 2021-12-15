a3c2e4658ad10467cbd7005e0be52f762713bb5e
christophermarek <christophermarek42@gmail.com>
2021-12-15 15:18:28 -0500

    sensible ml prediction finally

--- neural_network.py
+++ neural_network.py
@@ -10,57 +10,7 @@ from sklearn.model_selection import train_test_split
 # https://digitalcommons.usu.edu/cgi/viewcontent.cgi?article=2513&context=gradreports
 
 
-# 6 outputs one for each greek.
-# But thats later once I set this up properly
-def neural_network():
-
-    # Get dataset
-    filePath = 'output/demo_file.csv'
-
-    # output table of data showing few rows
-    df = False
-
-    try:
-        df = pd.read_csv(filePath)
-        print(df.head())
-    except:
-        print('error opening file')
-
-    X_train, X_test, y_train, y_test = train_test_split(df.drop('BS-Call', axis=1),
-                                                        df['BS-Call'], test_size=0.2)
-
-    # wonder if i should round output price column, we will see later
-
-    # What type of neural network would i use
-    # linear for now simple
-    # The nn.Sequential wraps the layers in the network. There are three linear layers with ReLU activation ( a simple function which allows
-    # positive values to pass through, whereas negative values are modified to zero )
-
-    #      The input
-    # layer has six nodes (one for each input parameter). The hidden layers have ten nodes each. The
-    # output layer has one node (for the predicted price)
-    input_size = 6
-    # first hidden layer 128 neurons, seconq 64
-    hidden_sizes = [10, 10]
-    output_size = 1
-    # input data passes through the model in sequential order, linear[hl1] -> relu -> linear[hl2] -> relu -> output -> softmax
-    model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                          nn.ReLU(),
-                          nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-                          nn.ReLU(),
-                          nn.Linear(hidden_sizes[1], output_size),
-                          nn.LogSoftmax)
-    print(model)
-
-    # https://stackoverflow.com/questions/46170814/how-to-train-pytorch-model-with-numpy-data-and-batch-size
-    # THis guy has a good way of setting up his code, and the guy answering explains how to do it right.
-    # he even links the docs, cause the data needs to be formatted a specific way for pytorhc
-
-    # https://github.com/pytorch/examples/blob/master/mnist/main.py#L37
-    # THis official example I can just copy most of it, it looks right too and can load local files
-    # like the other tutorial did instead of a slow to load df.
-# neural_network()
-
+# 6 outputs for 1 greek but thats later.
 
 class BlackScholesModel_Simple(nn.Module):
 
@@ -80,6 +30,27 @@ class BlackScholesModel_Simple(nn.Module):
         x = self.model(input)
         return x
 
+def trainModel(model, optimizer, lossFN, input, output, numEpochs):
+    model.train()
+    # begin to train
+    for i in range(numEpochs):
+        print('STEP: ', i)
+        def closure():
+            optimizer.zero_grad()
+            out = model(input.float())
+            loss = lossFN(out, output.float())
+            loss.backward()
+            return loss
+        optimizer.step(closure)
+
+def testModel(model, lossFN, testInput, testOutput):
+    # begin to predict, no need to track gradient here
+    model.eval()
+    with torch.no_grad():
+        pred = model(testInput.float())
+    cost = lossFN(pred, testOutput.float())
+    print("mean squared error:", cost.item())
+    return pred
 
 def main():
     # The "forward pass" refers to calculation process, values of the output layers from the inputs data. It's traversing through all neurons from first to last layer.
@@ -107,12 +78,14 @@ def main():
 
     # output table of data showing few rows
     df = False
-
     try:
         df = pd.read_csv(filePath)
-        # print(df.head())
     except:
         print('error opening file')
+
+    # Sort by time to maturity so model has sense of time?
+    # THis actually improves performance substantially, note this in description
+    df = df.sort_values(by=['timetomaturity'], ascending=True)
 
     X = df.drop(columns="BS-Call")
     y = df['BS-Call']
@@ -122,8 +95,6 @@ def main():
     # convert to tensors
     X_train = torch.tensor(X_train.values, dtype=torch.float64)
     X_test = torch.tensor(X_test.values, dtype=torch.float64)
-
-
     # Need to reshape y tensor so it has the same shape as X
     y_train = torch.tensor(y_train.values, dtype=torch.float64)
     y_test = torch.tensor(y_test.values, dtype=torch.float64)
@@ -135,45 +106,36 @@ def main():
 
     # build the model
     blackscholesmodel = BlackScholesModel_Simple()
-    # model = torch.load('model.ckpt')
+    model = torch.load('model.ckpt')
     # model.eval()
     criterion = nn.MSELoss()
+
     # use LBFGS as optimizer since we can load the whole data to train
     # lr = learning rate
     # Will need to test different learning rates and optimizers
     optimizer = optim.LBFGS(blackscholesmodel.parameters(), lr=0.5)
-    
-    #begin to train
-    for i in range(20):
-        print('STEP: ', i)
-        def closure():
-            optimizer.zero_grad()
-            out = blackscholesmodel(X_train.float())
-            loss = criterion(out, y_train.float())
-            # print('loss:', loss.item())
-            loss.backward()
-            return loss
-        optimizer.step(closure)
 
-    # After move my predictions to after the training
-    # begin to predict, no need to track gradient here
-    print(len(y_test.float()))
-    with torch.no_grad():
-        pred = blackscholesmodel(X_test.float())
-        # loss = criterion(pred, y_test.float())
-        # print('test loss:', loss.item())
-    cost = criterion(pred, y_test.float())
-    print("mean squared error:", cost.item())
-    # Now probably plot them to see.
+    # train model
+    trainModel(blackscholesmodel, optimizer, criterion, X_train, y_train, 20)
+    # test model and get output
+    pred = testModel(blackscholesmodel, criterion, X_test, y_test)
+    
+    # NOW NEED A WAY TO QUANTIFY ACCURACY BETTER, I LIKE THE TWO PLOTS BUT THEY STILL ARE NOT GOOD COPY YET
+    
+    # Then results need to be saved somewhere so i can compare all models
+
     # Maybe x axis is index
     # y axis is call value
     xAxis = []
     for n in range(3000):
         xAxis.append(n)
 
-    plt.scatter(xAxis, y_test.float()[0:3000],marker=".", label="ytest", color='black')
-    plt.scatter(xAxis, pred[0:3000],marker=".", label="ml prediction", color='red')
-    plt.legend()
+    fig, ax = plt.subplots(2, 1)
+
+    ax[0].scatter(xAxis, pred[0:3000],marker=".", label="ml prediction", color='red')
+    ax[0].scatter(xAxis, y_test.float()[0:3000],marker=".", label="y_test", color='black')
+    ax[0].legend()
+    ax[1].scatter(y_test.float()[0:3000], pred[0:3000],marker=".")
     plt.show()
 
     # Save the model checkpoint
