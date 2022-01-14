dc7b752eed61aeae19aa6f6d4fdb2383a087d933
christophermarek <christophermarek42@gmail.com>
2022-01-14 11:40:54 -0500

    test model instruction added

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from torch import nn, optim
 import os
 import torch
 
-#local imports
+# local imports
 import data_generator
 import models as mlModelsClass
 
-from neural_network import loadData, trainModel
+from neural_network import loadData, trainModel, testModel
+
+
+def get_model(model_name):
+    runningModel = False
+    if model_name == 'BlackScholesModel_Simple_Greeks':
+        runningModel = mlModelsClass.BlackScholesModel_Simple_Greeks()
+    return runningModel
 
 
 def instruction_generate_dataset(input):
@@ -15,90 +22,116 @@ def instruction_generate_dataset(input):
 
     dataset_title = input[0]
     dataset_size = int(input[1])
-    
+
     dataGenerator = data_generator.DataGenerator(dataset_title)
     output = dataGenerator.generateDataSet(dataset_size)
-        
+
     print(output[1])
     if not output[0]:
         return
-    
+
     print('INSTRUCTION COMPLETE: Generate Dataset \n')
 
 
 def instruction_train_model(input):
-    print('INSTRUCTION Train Model')    
-    
+    print('INSTRUCTION Train Model')
+
     dataset_path = input[0]
     model_name = input[1]
     model_init_params = input[2]
     learning_rate = float(input[3])
     num_epochs = int(input[4])
-    
+
     # not a config entry yet
     criterion = nn.MSELoss()
-    
-    # load data into training set, maybe move into main?
+
     dataSet = loadData(dataset_path)
-    if not dataSet: 
+    if not dataSet:
         print('invalid dataset path')
         return
-    
+
     X_train = dataSet[0]
     y_train = dataSet[2]
-    
-    runningModel = False
-    if model_name == 'BlackScholesModel_Simple_Greeks': 
-        runningModel = mlModelsClass.BlackScholesModel_Simple_Greeks()
-        
+
+    runningModel = get_model(model_name)
     if not runningModel:
         print('INSTRUCTION FAILED: Train Model')
-        return 
-    
+        return
+
     # use LBFGS as optimizer since we can load the whole data to train
     # lr = learning rate
     # Will need to test different learning rates and optimizers
     optimizer = optim.LBFGS(runningModel.parameters(), lr=learning_rate)
 
     # train model
-    trainModel(runningModel, optimizer, criterion, X_train, y_train, num_epochs)
+    trainModel(runningModel, optimizer, criterion,
+               X_train, y_train, num_epochs)
     if not os.path.exists('models'):
         os.makedirs('models')
     # will overwrite existing trained model
     # can leave this code in the train model in nn to remove imports from this file
-    torch.save(runningModel.state_dict(), "models/" + 'model_name'  + '.ckpt')
-    
-    print('INSTRUCTION Complete: Train Model \n')    
-    
-    
-def main():
-    
-    print("Program Started \n")
-    
-    print("Loading Config \n")
-    path_to_config = 'config.txt'
-    if not path_to_config: 
-        print('invalid path to config')
+    torch.save(runningModel.state_dict(), "models/" + model_name + '.ckpt')
+
+    print('INSTRUCTION Complete: Train Model \n')
+
+
+def instruction_test_model(input):
+    print('INSTRUCTION TEST Model')
+
+    dataset_path = input[0]
+    model_name = input[1]
+    model_path = input[2]
+
+    dataSet = loadData(dataset_path)
+    if not dataSet:
+        print('INSTRUCTION FAILED: TEST MODEL, invalid dataset path')
         return
     
+    X_test = dataSet[1]
+    y_test = dataSet[3]
+
+    runningModel = get_model(model_name)
+    if not runningModel:
+        print('INSTRUCTION FAILED: TEST MODEL, invalid model_name')
+        return
+    runningModel.load_state_dict(torch.load("models/" + model_path))
+  
+
+    criterion = nn.MSELoss()
+    pred, error = testModel(runningModel, criterion, X_test, y_test)
+    print(error)
+    
+    print('INSTRUCTION Complete: TEST Model \n')
+    
+
+
+def main():
+
+    print("Program Started \n")
+
+    print("Loading Config \n")
+    path_to_config = 'config.txt'
+    if not path_to_config:
+        print('invalid path to config')
+        return
+
     print("Reading Config \n")
     with open(path_to_config) as file:
-        
+
         inComment = False
         inInstruction = False
         capturedLines = list()
         processFunction = None
-                
+
         for line in file:
             if line.strip() == '':
                 continue
-            
+
             if line.strip() == '!':
                 inComment = not inComment
                 continue
-            
+
             if not inComment:
-                
                 if not inInstruction:
                     # Capture instruction type
                     if line.strip() == 'GENERATEDATASET':
@@ -107,8 +140,10 @@ def main():
                     if line.strip() == 'TRAIN':
                         processFunction = instruction_train_model
                         inInstruction = True
+                    if line.strip() == 'TEST':
+                        processFunction = instruction_test_model
+                        inInstruction = True
 
-                    
                 else:
                     if line.strip() == 'END':
                         processFunction(capturedLines)
@@ -117,8 +152,9 @@ def main():
                         capturedLines = list()
                     else:
                         capturedLines.append(line.strip())
-                    
+
     print("Completed Going Through Config \n")
-    
+
+
 if __name__ == "__main__":
     main()
--- neural_network.py
+++ neural_network.py
@@ -23,7 +23,7 @@ import os
 # load data into train set
 def loadData(fileDir):
     fileDir = 'generated_datasets/' + fileDir + '.csv'
-    
+
     # load data and make training set
     # Get dataset
     filePath = fileDir
@@ -39,13 +39,14 @@ def loadData(fileDir):
     df = df.sort_values(by=['timetomaturity'], ascending=True)
     # print('PRINTING FIRST 5 ROWS OF DATASET')
     # print(df.head())
-    
+
     # price not used for greeks, but it is actually.
     df = df.drop(columns="BS-Call")
-    
-    X = df.drop(columns=['iv','delta', 'gamma', 'rho', 'theta', 'vega'])
-    y = df[['iv','delta', 'gamma', 'rho', 'theta', 'vega']]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    X = df.drop(columns=['iv', 'delta', 'gamma', 'rho', 'theta', 'vega'])
+    y = df[['iv', 'delta', 'gamma', 'rho', 'theta', 'vega']]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
 
     # convert to tensors
     X_train = torch.tensor(X_train.values, dtype=torch.float64)
@@ -61,12 +62,12 @@ def loadData(fileDir):
     return [X_train, X_test, y_train, y_test]
 
 
-
 def trainModel(model, optimizer, lossFN, input, output, numEpochs):
     model.train()
     # begin to train
     for i in range(numEpochs):
         print('Epoch: ', i)
+
         def closure():
             optimizer.zero_grad()
             out = model(input.float())
@@ -74,6 +75,7 @@ def trainModel(model, optimizer, lossFN, input, output, numEpochs):
             loss.backward()
             return loss
         optimizer.step(closure)
+
 
 def testModel(model, lossFN, testInput, testOutput):
     # begin to predict, no need to track gradient here
@@ -83,6 +85,7 @@ def testModel(model, lossFN, testInput, testOutput):
     cost = lossFN(pred, testOutput.float())
     print("mean squared error:", cost.item())
     return [pred, cost.item()]
+
 
 def main(trainOrTestMode, models, dataSet, hyperparam_config):
     # The "forward pass" refers to calculation process, values of the output layers from the inputs data. It's traversing through all neurons from first to last layer.
@@ -101,130 +104,71 @@ def main(trainOrTestMode, models, dataSet, hyperparam_config):
     # do this so results can be reproducable
     np.random.seed(0)
     torch.manual_seed(0)
-    
-    # Add note how relu makes models predict 0, especially with more layers. This is probably because the relu forces it to 0.
-    
-    # ADD a way to load multiple datasets
-    
-    X_train, X_test, y_train, y_test = dataSet
-    print('in main neural_networks now')
-    models_error = list()
-    
+
     for model in models:
-        for hyperParamConfigEntry in hyperparam_config:
-            learningRate = hyperParamConfigEntry[0]
-            numNeurons = hyperParamConfigEntry[1]
 
-            # build the model
-            runningModel = False
-            # else:
-            #     print(model)
-            #     modelSplit = model.split(',')
-            #     model_name = modelSplit[0]
-            #     restOfModel = ''.join(modelSplit[1:])
-            #     restOfModelSplit = restOfModel.split('.')
-            #     numNeurons = int(restOfModelSplit[1][-2:])
-            #     learningRate = restOfModelSplit[1][:-2]
-            #     learningRate = float('0.' + learningRate)
-            #     if model_name == 'simpleblackscholes': runningModel = mlModelsClass.BlackScholesModel_Simple(numNeurons)
-            #     elif model_name == 'simpleblackscholes2layer': runningModel = mlModelsClass.BlackScholesModel_Simple2Layer(numNeurons)
-            #     elif model_name == 'simpleblackscholes3layer': runningModel = mlModelsClass.BlackScholesModel_Simple3Layer(numNeurons)
-            #     elif model_name == 'simpleblackscholes4layer': runningModel = mlModelsClass.BlackScholesModel_Simple4Layer(numNeurons)
-            #     elif model_name == 'simpleblackscholesgreeks': runningModel = mlModelsClass.BlackScholesModel_Simple_Greeks(numNeurons)
-                
-            if not model: 
-                print('invalid model name passed')
-                return
-            if(trainOrTestMode == 1):
-                runningModel.load_state_dict(torch.load("models/" + model))
+        models_error.append([model, error])
+        # Then results need to be saved somewhere so i can compare all models
+        now = datetime.now()
 
+        # # Save plots to an output dir, title model name and date/time ran
+        if not os.path.exists('model_output'):
+            os.makedirs('model_output')
+        if not os.path.exists('model_output/' + model):
+            os.makedirs('model_output/' + model)
+        if not os.path.exists('model_output/' + model + '/' + now.strftime("%d_%m_%Y_%H_%M_%S")):
+            os.makedirs('model_output/' + model + '/' +
+                        now.strftime("%d_%m_%Y_%H_%M_%S"))
 
+        # xAxis = []
+        # for n in range(len(pred)):
+        #     xAxis.append(X_test[n][1])
 
+        # figure out this straight line issue, its because of the batching i think.
+        # AM i mutatating data instead of copying it I am not sure.
+        # just lower epochs so I can test it way quicker
+        # and then do them individually
 
-            # test model and get output
-            if trainOrTestMode == 1 or trainOrTestMode == 2:
-                criterion = nn.MSELoss()
-                pred, error = testModel(runningModel, criterion, X_test, y_test)
-                models_error.append([model,error])
-                # Then results need to be saved somewhere so i can compare all models
-                now = datetime.now()
-                
-                # # Save plots to an output dir, title model name and date/time ran
-                if not os.path.exists('model_output'):
-                    os.makedirs('model_output')
-                if not os.path.exists('model_output/' + model):
-                    os.makedirs('model_output/' + model)
-                if not os.path.exists('model_output/' + model + '/' + now.strftime("%d_%m_%Y_%H_%M_%S")):
-                    os.makedirs('model_output/' + model + '/' + now.strftime("%d_%m_%Y_%H_%M_%S"))
-                
-                # xAxis = []
-                # for n in range(len(pred)):
-                #     xAxis.append(X_test[n][1])
-                    
-                # figure out this straight line issue, its because of the batching i think.
-                # AM i mutatating data instead of copying it I am not sure.
-                # just lower epochs so I can test it way quicker
-                # and then do them individually
+        # plt.clf()
+        # plt.scatter(xAxis, pred[0:len(pred)],marker=".", label="ml prediction", color='red')
+        # plt.scatter(xAxis, y_test.float()[0:len(pred)],marker=".", label="y_test", color='black')
+        # plt.xlabel('Time To Maturity')
+        # plt.ylabel('Option Price')
+        # plt.legend()
+        # plt.savefig('model_output/' + model + "/" + now.strftime("%d_%m_%Y_%H_%M_%S") + "/" + str(learningRate) + "," + str(numNeurons) + "vsMaturity" + '.png')
+        plt.clf()
+        # plt.scatter(y_test.float()[0:len(pred)], pred[0:len(pred)],marker=".")
+        # plt.xlabel('Test Option Price')
+        # plt.ylabel('Predicted Option Price')
+        # plt.savefig('model_output/' + model + "/" +  now.strftime("%d_%m_%Y_%H_%M_%S") + "/" + str(learningRate) + "," +str(numNeurons) + "testvsprediction" + '.png')
+        print(pred)
+        predIv, predDelta, predGamma, predRho, predTheta, predVega, testIv, testDelta, testGamma, testRho, testTheta, testVega = list(
+        ), list(), list(), list(), list(), list(), list(), list(), list(), list(), list(), list()
+        for i in range(len(pred)):
+            predIv = pred[i][0]
+            testIv = y_test[i][0]
+            predDelta = pred[i][1]
+            testDelta = y_test[i][1]
+            predGamma = pred[i][2]
+            testGamma = y_test[i][2]
+            predRho = pred[i][3]
+            testRho = y_test[i][3]
+            predTheta = pred[i][4]
+            testTheta = y_test[i][4]
+            predVega = pred[i][5]
+            testVega = y_test[i][5]
 
-                # plt.clf()
-                # plt.scatter(xAxis, pred[0:len(pred)],marker=".", label="ml prediction", color='red')
-                # plt.scatter(xAxis, y_test.float()[0:len(pred)],marker=".", label="y_test", color='black')
-                # plt.xlabel('Time To Maturity')
-                # plt.ylabel('Option Price')
-                # plt.legend()
-                # plt.savefig('model_output/' + model + "/" + now.strftime("%d_%m_%Y_%H_%M_%S") + "/" + str(learningRate) + "," + str(numNeurons) + "vsMaturity" + '.png')
-                plt.clf()
-                # plt.scatter(y_test.float()[0:len(pred)], pred[0:len(pred)],marker=".")
-                # plt.xlabel('Test Option Price')
-                # plt.ylabel('Predicted Option Price')
-                # plt.savefig('model_output/' + model + "/" +  now.strftime("%d_%m_%Y_%H_%M_%S") + "/" + str(learningRate) + "," +str(numNeurons) + "testvsprediction" + '.png')
-                print(pred)
-                predIv, predDelta, predGamma, predRho, predTheta, predVega, testIv, testDelta, testGamma, testRho, testTheta, testVega = list(), list(), list(), list(), list(), list(), list(), list(), list(), list(), list(), list()
-                for i in range(len(pred)):
-                    predIv = pred[i][0]
-                    testIv = y_test[i][0]
-                    predDelta = pred[i][1]
-                    testDelta = y_test[i][1]
-                    predGamma = pred[i][2]
-                    testGamma = y_test[i][2]
-                    predRho = pred[i][3]
-                    testRho = y_test[i][3]
-                    predTheta = pred[i][4]
-                    testTheta = y_test[i][4]
-                    predVega = pred[i][5]
-                    testVega = y_test[i][5]
-                
-                fig, axs = plt.subplots(2, 3)
-                    
-                axs[0][0].plot(testIv, predIv)
-                axs[0][1].plot(testDelta, predDelta)
-                axs[0][2].plot(testGamma, predGamma)
-                axs[1][0].plot(testRho, predRho)
-                axs[1][1].plot(testTheta, predTheta)
-                axs[1][2].plot(testVega, predVega)
+        fig, axs = plt.subplots(2, 3)
 
-                plt.show()
-                
-                # doesnt work wtf, try plotting individual ones i guess?
-                
-                plt.clf()
-              
-    # Then sort lowest to highest and display
-    models_error.sort(key = lambda x: x[1])
-                
-    # write to file list of models and their mse
-    f = open("model_output/modelMse.csv", "w")
-    f.write("model,mse\n")
-    for i in range(len(models_error)):
-        f.write(models_error[i][0] + "," + str(models_error[i][1]) + "\n")
-    f.close()
-    # show model with lowest mse
-    
-    # write to a separate file model with the lowest mse
-    f = open("model_output/lowesterrormodel.", "w")
-    f.write(models_error[0][0] + "," + str(models_error[0][1]))
-    f.close()           
-       
-       
-       
-            
+        axs[0][0].plot(testIv, predIv)
+        axs[0][1].plot(testDelta, predDelta)
+        axs[0][2].plot(testGamma, predGamma)
+        axs[1][0].plot(testRho, predRho)
+        axs[1][1].plot(testTheta, predTheta)
+        axs[1][2].plot(testVega, predVega)
+
+        plt.show()
+
+        # doesnt work wtf, try plotting individual ones i guess?
+
+        plt.clf()
